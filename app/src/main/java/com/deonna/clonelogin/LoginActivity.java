8efe3cabd4181b269c4bde6033db99698f31c146
Deonna Hodges <deonna@learnersguild.org>
2017-03-10 16:26:09 -0800

    feat(clone-login): Cloned Facebook login UI.

---
+++ app/src/main/java/com/deonna/clonelogin/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.deonna.clonelogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnLogin) Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void openStream() {
+
+        Intent intent = new Intent(this, StreamActivity.class);
+        startActivity(intent);
+    }
+}
